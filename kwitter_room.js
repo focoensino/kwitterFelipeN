8af8e48a255602d1eeb40650fd722ec907601b7a
Sérgio Tafine <focoensinoonline@gmail.com>
2023-12-21 14:14:57 -0300

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,12 +1,6 @@
   //Configurações de seu App Firebase
   const firebaseConfig = {
-    apiKey: "AIzaSyDlmkW0HtzuUZQR5YCsCf9uaJ1Ijom5PUY",
-    authDomain: "twiter-da-shoppe.firebaseapp.com",
-    databaseURL: "https://twiter-da-shoppe-default-rtdb.firebaseio.com",
-    projectId: "twiter-da-shoppe",
-    storageBucket: "twiter-da-shoppe.appspot.com",
-    messagingSenderId: "213700606823",
-    appId: "1:213700606823:web:27ee7368a74bd04d788dc2"
+   
   
   };
   
@@ -19,15 +13,7 @@
   
   function addRoom()
   {
-    room_name = document.getElementById("room_name").value;
   
-    firebase.database().ref("/").child(room_name).update({
-      purpose : "adicionando nome da sala"
-    });
-  
-      localStorage.setItem("room_name", room_name);
-      
-      window.location = "kwitter_page.html";
   }
   
   function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
@@ -44,14 +30,10 @@
   
   function redirectToRoomName(name)
   {
-    console.log(name);
-    localStorage.setItem("room_name", name);
-      window.location = "kwitter_page.html";
+   
   }
   
   function logout() {
-  localStorage.removeItem("user_name");
-  localStorage.removeItem("room_name");
-      window.location = "index.html";
+  
   }
   
